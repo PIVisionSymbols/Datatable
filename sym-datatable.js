9b4167dbab736e096f5a7019b01a11deedccfad9
jefftwhite <56368619+jefftwhite@users.noreply.github.com>
2019-10-09 15:03:34 -0600

    Add files via upload

---
+++ sym-datatable.js
@@ -0,0 +1,55 @@
+(function (PV) {
+	"use strict";
+
+	function symbolVis() { };
+	PV.deriveVisualizationFromBase(symbolVis);
+
+	var definition = { 
+		typeName: "datatable",
+		displayName: 'Data Table',
+		iconUrl: 'Scripts/app/editor/symbols/ext/images/mountainwest.png',
+		visObjectType: symbolVis,
+		datasourceBehavior: PV.Extensibility.Enums.DatasourceBehaviors.Single,
+		getDefaultConfig: function(){ 
+			return { 
+				DataShape: 'Timeseries',
+				Height: 150,
+				Width: 150, 
+				HeaderColor: "blue",
+				
+			} 
+		},
+		configOptions: function(){
+			return[
+				{
+					title: "Format Symbol",
+					mode: "format"
+					
+				}
+			];
+		}
+	}
+	
+	
+	symbolVis.prototype.init = function(scope, elem) { 
+		this.onDataUpdate = dataUpdate;
+
+		function dataUpdate(data){
+			if(!data) return;
+			
+			scope.Values=data.Data[0].Values;
+
+			//sporadic update data
+			if(data.Data[0].Label){
+				scope.Label=data.Data[0].Label;
+			}
+		}
+		
+		
+	};
+	
+	
+
+	PV.symbolCatalog.register(definition); 
+
+})(window.PIVisualization); 
